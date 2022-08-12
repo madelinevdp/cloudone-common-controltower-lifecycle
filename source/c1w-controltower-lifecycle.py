09aae13050052c6c698438bede8c8887d9bf8fcc
Madeline Van Der Paelt (RD-CA) <madeline_van_der_paelt@trendmicro.com>
2022-08-12 12:23:48 -0400

    Working Control Tower integration for C1 - add and delete

--- source/c1w-controltower-lifecycle.py
+++ source/c1w-controltower-lifecycle.py
@@ -8,6 +8,7 @@ import c1wconnectorapi
 import c1wresources
 import cfnhelper
 import ctlifecycleevent
+import requests
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -18,73 +19,131 @@ def get_org_id():
     return client.list_roots()["Roots"][0]['ARN'].rsplit(':')[4]
 
 
-def create_cross_account_role(aws_account_id):
+def create_oidc_provider(aws_account_id):
     sts_session = assume_role(aws_account_id, c1wresources.ControlTowerRoleName)
     client = sts_session.client('iam')
     logger.info(
-        f'Creating role {c1wresources.IamRoleName} and policy {c1wresources.IamPolicyName} in account {aws_account_id}')
+        f'Creating role {c1wresources.IamRoleName}, policies {c1wresources.IamPolicyName1} & {c1wresources.IamPolicyName2}, and OIDC provider {c1wresources.OIDCProviderUrl} in account {aws_account_id}')
     path = "/"
     try:
         logger.info('Creating role...')
         client.create_role(Path=path, RoleName=c1wresources.IamRoleName,
-                           AssumeRolePolicyDocument=c1wresources.get_assume_role_policy_document(),
-                           Description='CloudOne Workload Connector Role created by Control Tower Master'
+                           AssumeRolePolicyDocument=c1wresources.get_assume_role_policy_document(aws_account_id, c1wresources.OIDCProviderUrl),
+                           Description='CloudOne Integration Role created by Control Tower Master'
                            )
     except Exception as e:
         logger.info('Failed to create role')
         logger.info(e)
         raise e
     try:
-        logger.info('Creating policy...')
-        client.create_policy(PolicyName=c1wresources.IamPolicyName,
-                             PolicyDocument=json.dumps(c1wresources.policy_document)
+        logger.info('Creating policy 1...')
+        client.create_policy(PolicyName=c1wresources.IamPolicyName1,
+                             PolicyDocument=json.dumps(c1wresources.policy_document_1)
                              )
     except Exception as e:
-        logger.info('Failed to create policy')
+        logger.info('Failed to create policy 1')
         logger.info(e)
         raise e
     try:
-        logger.info('Attaching policy...')
-        client.attach_role_policy(PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName}',
+        logger.info('Attaching policy 1...')
+        client.attach_role_policy(PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName1}',
                                   RoleName=c1wresources.IamRoleName
                                   )
     except Exception as e:
-        logger.info('Failed to attach policy')
+        logger.info('Failed to attach policy 1')
+        logger.info(e)
+        raise e
+    try:
+        logger.info('Creating policy 2...')
+        client.create_policy(PolicyName=c1wresources.IamPolicyName2,
+                             PolicyDocument=json.dumps(c1wresources.policy_document_2)
+                             )
+    except Exception as e:
+        logger.info('Failed to create policy 2')
+        logger.info(e)
+        raise e
+    try:
+        logger.info('Attaching policy 2...')
+        client.attach_role_policy(PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName2}',
+                                  RoleName=c1wresources.IamRoleName
+                                  )
+    except Exception as e:
+        logger.info('Failed to attach policy 2')
+        logger.info(e)
+        raise e
+    try:
+        logger.info('Creating OIDC provider...')
+        client.create_open_id_connect_provider(
+            Url='https://74u3z7zmn1.execute-api.ca-central-1.amazonaws.com/dev-mvdpxx',
+            ClientIDList=[f'arn:aws:iam::{aws_account_id}:root'],
+            ThumbprintList=['9e99a48a9960b14926bb7f3b02e22da2b0ab7280'])
+    except Exception as e:
+        logger.info('Failed to create OIDC provider')
         logger.info(e)
         raise e
     else:
         return True
 
 
-def delete_cross_account_role(aws_account_id):
+def delete_oidc_provider(aws_account_id):
     sts_session = assume_role(aws_account_id, c1wresources.ControlTowerRoleName)
     client = sts_session.client('iam')
     logger.info(f'Account is {boto3.client("sts").get_caller_identity()["Account"]}')
     try:
+        logger.info(f'Detatching {c1wresources.IamPolicyName1}')
         client.detach_role_policy(
-            PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName}',
+            PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName1}',
             RoleName=c1wresources.IamRoleName
         )
-        logger.info('Detached policy')
+        logger.info(f'Detached {c1wresources.IamPolicyName1}')
     except Exception as e:
         logger.info(
-            f'Failed to detach policy {c1wresources.IamPolicyName} from role {c1wresources.IamRoleName} \
+            f'Failed to detach policy {c1wresources.IamPolicyName1} from role {c1wresources.IamRoleName} \
                 in account {aws_account_id}')
         logger.info(e)
     try:
+        logger.info(f'Detatching {c1wresources.IamPolicyName2}')
+        client.detach_role_policy(
+            PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName2}',
+            RoleName=c1wresources.IamRoleName
+        )
+        logger.info(f'Detached {c1wresources.IamPolicyName2}')
+    except Exception as e:
+        logger.info(
+            f'Failed to detach policy {c1wresources.IamPolicyName2} from role {c1wresources.IamRoleName} \
+                in account {aws_account_id}')
+        logger.info(e)
+    try:
+        logger.info(f'Deleting {c1wresources.IamRoleName}')
         client.delete_role(RoleName=c1wresources.IamRoleName)
         logger.info('Deleted role')
     except Exception as e:
         logger.info(f'Failed to delete role {c1wresources.IamRoleName} in account {aws_account_id}')
         logger.info(e)
     try:
-        client.delete_policy(PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName}')
+        logger.info(f'Deleting {c1wresources.IamPolicyName1}')
+        client.delete_policy(PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName1}')
         logger.info('Deleted policy')
     except Exception as e:
         logger.info(
             "Failed to delete policy: {arn}".format(
-                arn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName}'))
+                arn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName1}'))
         logger.info(e)
+    try:
+        logger.info(f'Deleting {c1wresources.IamPolicyName2}')
+        client.delete_policy(PolicyArn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName2}')
+        logger.info('Deleted policy')
+    except Exception as e:
+        logger.info(
+            "Failed to delete policy: {arn}".format(
+                arn=f'arn:aws:iam::{aws_account_id}:policy/{c1wresources.IamPolicyName2}'))
+        logger.info(e)
+    try:
+        logger.info(f'Deleting OIDC provider {c1wresources.OIDCProviderUrl}')
+        client.delete_open_id_connect_provider(OpenIDConnectProviderArn=f'arn:aws:iam::{aws_account_id}:oidc-provider/{c1wresources.OIDCProviderUrl}')
+        logger.info('Deleted OIDC provider')
+    except Exception as e:
+        logger.info(f'Failed to delete OIDC provider {c1wresources.OIDCProviderUrl} in account {aws_account_id}')
     return
 
 
@@ -117,35 +176,55 @@ def assume_role(aws_account_number, role_name) -> boto3.Session:
         raise e
 
 
-def configure_account(aws_account_id, aws_account_name):
+def configure_account(aws_account_id):
     c1w_connector = c1wconnectorapi.CloudOneConnector(c1wresources.get_api_key())
     iam_client = boto3.client('iam')
     try:
-        logger.info('Create Connector Object')
-        logger.info('Create role in target account')
-        create_cross_account_role(aws_account_id)
+        logger.info('Create Cloud One Integration')
+        logger.info('Create OIDC resources in target account')
+        create_oidc_provider(aws_account_id)
         time.sleep(20)
-        logger.info('Add account to Cloud One Workload')
     except iam_client.exceptions.EntityAlreadyExistsException as e:
-        update_policy(aws_account_id)
+        logger.info("entity already exists")
+        # update_policy(aws_account_id)
     except Exception as e:
         logger.info(f'Failed to configure account {aws_account_id} with exception: {e}')
     try:
-        return c1w_connector.add_connector(f'arn:aws:iam::{aws_account_id}:role/{c1wresources.IamRoleName}',
-                                           aws_account_id, aws_account_name)
+        logger.info('Add integration to Cloud One')
+        # return c1w_connector.add_connector(f'arn:aws:iam::{aws_account_id}:role/{c1wresources.IamRoleName}')
+        #TODO get api key from secret
+        c1_api_key = c1wresources.get_api_key()
+        body = {'roleArn': f'arn:aws:iam::{aws_account_id}:role/{c1wresources.IamRoleName}'}
+        bodyJson = json.dumps(body)
+        response = requests.post(
+            'https://74u3z7zmn1.execute-api.ca-central-1.amazonaws.com/dev-mvdpxx/api/cloudaccounts/aws', 
+            data = bodyJson,
+            headers = {'Api-Version': 'v1', 'Authorization': f'ApiKey {c1_api_key}'}
+        )
+        logger.info(response)
     except Exception as e:
         logger.info(f'Failed to add workload connector with exception {e}')
 
 
 def remove_account_config(aws_account_id):
+       
     try:
-        c1w_connector = c1wconnectorapi.CloudOneConnector(c1wresources.get_api_key())
-        logger.info(f'Removing account from c1w')
-        c1w_connector.delete_connector(aws_account_id)
-        logger.info('Removing role from target account')
-        delete_cross_account_role(aws_account_id)
+        logger.info(f'Removing Cloud One integration')
+        # return c1w_connector.add_connector(f'arn:aws:iam::{aws_account_id}:role/{c1wresources.IamRoleName}')
+        c1_api_key = c1wresources.get_api_key()
+        response = requests.delete(
+            f'https://74u3z7zmn1.execute-api.ca-central-1.amazonaws.com/dev-mvdpxx/api/cloudaccounts/aws/{aws_account_id}', 
+            headers = {'Api-Version': 'v1', 'Authorization': f'ApiKey {c1_api_key}'}
+        )
+        logger.info(response)
     except Exception as e:
-        logger.info(f'Failed to remove account {aws_account_id} config with exception: {e}')
+        logger.info(f'Failed to delete Cloud One integration with exception {e}')
+    try:
+        logger.info('Removing OIDC resources from target account')
+        delete_oidc_provider(aws_account_id)
+        logger.info(f'Removed OIDC resources from {aws_account_id}')
+    except Exception as e:
+        logger.info(f'Failed to remove OIDC resources from {aws_account_id} with exception: {e}')
 
 
 def update_policy(aws_account_id, aws_account_name):
@@ -164,7 +243,7 @@ def update_policy(aws_account_id, aws_account_name):
     try:
         update_assume_role_response = client.update_assume_role_policy(
             RoleName=c1wresources.IamRoleName,
-            PolicyDocument=c1wresources.get_assume_role_policy_document()
+            PolicyDocument=c1wresources.get_assume_role_policy_document(aws_account_id, c1wresources.OIDCProviderUrl)
         )
     except Exception as e:
         logger.info(f'Failed to update AssumeRolePolicyDocument: {e}')
@@ -207,7 +286,7 @@ def fresh_deploy(function_name):
         client.invoke(
             FunctionName=function_name,
             InvocationType='Event',
-            Payload=json.dumps({'InvokeAction': 'configure_account', 'account_id': account_id, 'account_name': account_name})
+            Payload=json.dumps({'InvokeAction': 'configure_account', 'account_id': account_id})
         )
         count += 1
     print(f'Launched configure_account for {count} accounts')
@@ -233,7 +312,7 @@ def remove_all(function_name):
     client = boto3.client('lambda')
     logger.info(f'Received function name {function_name} from context')
     count = 0
-    for account_id in get_accounts():
+    for account_id, account_name in get_accounts():
         client.invoke(
             FunctionName=function_name,
             InvocationType='Event',
@@ -259,24 +338,26 @@ def lambda_handler(event, context):
                 logger.info(f'Failed to handle create event with exception: {e}')
                 response.send(cfnhelper.responseCode.FAILED)
             response.send(cfnhelper.responseCode.SUCCESS)
-        elif event['RequestType'] == 'Update':
-            logger.info(f"Received CFN update")
-            response = cfnhelper.cfnResponse(event, context)
-            try:
-                update_accounts(context.function_name)
-            except Exception as e:
-                logger.info(f'Failed to handle update event with exception: {e}')
-                response.send(cfnhelper.responseCode.FAILED)
-            response.send(cfnhelper.responseCode.SUCCESS)
+        # elif event['RequestType'] == 'Update':
+        #     logger.info(f"Received CFN update")
+        #     response = cfnhelper.cfnResponse(event, context)
+        #     try:
+        #         update_accounts(context.function_name)
+        #     except Exception as e:
+        #         logger.info(f'Failed to handle update event with exception: {e}')
+        #         response.send(cfnhelper.responseCode.FAILED)
+        #     response.send(cfnhelper.responseCode.SUCCESS)
         else:
             logger.info(f"Ignoring unhandled cfn request type: {event['RequestType']}")
             response = cfnhelper.cfnResponse(event, context)
             response.send(cfnhelper.responseCode.SUCCESS)
     elif 'InvokeAction' in event:
         if event['InvokeAction'] == 'configure_account':
-            configure_account(event['account_id'], event['account_name'])
-        elif event['InvokeAction'] == 'update_account':
-            update_policy(event['account_id'], event['account_name'])
+            configure_account(event['account_id'])
+        elif event['InvokeAction'] == 'configure_all':
+            fresh_deploy(context.function_name)
+        # elif event['InvokeAction'] == 'update_account':
+        #     update_policy(event['account_id'], event['account_name'])
         elif event['InvokeAction'] == 'remove_account_config':
             remove_account_config(event['account_id'])
         elif event['InvokeAction'] == 'remove_all':
@@ -288,7 +369,7 @@ def lambda_handler(event, context):
             logger.info(f'Did not find a supported event')
             return
         if life_cycle_event.create_account:
-            configure_account(life_cycle_event.child_account_id, life_cycle_event.child_account_name)
+            configure_account(life_cycle_event.child_account_id)
         elif life_cycle_event.event_name == 'RemoveAccount':
             remove_account_config(life_cycle_event.child_account_id)
         else:
--- source/c1wconnectorapi.py
+++ source/c1wconnectorapi.py
@@ -9,34 +9,49 @@ logger = logging.getLogger()
 class CloudOneConnector:
     def __init__(self, api_key):
         self.configuration = deepsecurity.Configuration()
-        self.configuration.host = f'https://{os.environ["ApiEndpoint"]}/api'
+        self.configuration.host = f'https://74u3z7zmn1.execute-api.ca-central-1.amazonaws.com/dev-mvdpxx/api'
+        self.oidcProvider = '74u3z7zmn1.execute-api.ca-central-1.amazonaws.com/dev-mvdpxx'
         self.configuration.api_key['api-secret-key'] = api_key
         self.connectorClient = deepsecurity.AWSConnectorsApi(deepsecurity.ApiClient(self.configuration))
         self.apiVersion = 'v1'
 
-    def get_externalid(self):
-        connector_settings_client = deepsecurity.AWSConnectorSettingsApi(deepsecurity.ApiClient(self.configuration))
+    def add_connector(self, role_arn):
         try:
-            connector_settings = connector_settings_client.list_aws_connector_settings(self.apiVersion)
-            logger.info('Retrieved external id')
-            return connector_settings.external_id
-        except ApiException as e:
-            logger.info(f"Exception when calling AWSConnectorSettingsApi.list_aws_connector_settings: {e}")
-            raise e
-        except Exception as e:
-            logger.info(e)
+            params = locals()
+            collection_formats = {}
+            path_params = {}
+            query_params = []
+            header_params = {}
+            header_params['api-version'] = "v1"  # noqa: E501
+            form_params = []
+            local_var_files = {}
+            body_params = {
+                'roleArn': role_arn
+            }
+            # HTTP header `Accept`
+            header_params['Accept'] = self.api_client.select_header_accept(
+                ['application/json'])  # noqa: E501
+            # HTTP header `Content-Type`
+            header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+                ['application/json'])  # noqa: E501
+            # Authentication setting
+            auth_settings = ['DefaultAuthentication']  # noqa: E501
 
-    def add_connector(self, aws_cross_account_arn, aws_account_id, aws_account_name):
-        if aws_account_name:
-            display_name = f"ControlTower - {aws_account_name} - {aws_account_id}"
-        else:
-            display_name = f"ControlTower - {aws_account_id}"
-        aws_connector = deepsecurity.AWSConnector(cross_account_role_arn=aws_cross_account_arn,
-                                                  workspaces_enabled="True",
-                                                  display_name=display_name)
-        logger.info(f'Creating connector for arn: {aws_cross_account_arn}')
-        try:
-            add_connector_response = self.connectorClient.create_aws_connector(aws_connector, self.apiVersion)
+            add_connector_response = self.connectorClient.api_client.call_api(
+                'cloudaccounts/aws', "POST",
+                path_params,
+                query_params,
+                header_params,
+                body=body_params,
+                post_params=form_params,
+                files=local_var_files,
+                response_type='AWSConnector',  # noqa: E501
+                auth_settings=auth_settings,
+                async_req=params.get('async_req'),
+                _return_http_data_only=params.get('_return_http_data_only'),
+                _preload_content=params.get('_preload_content', True),
+                _request_timeout=params.get('_request_timeout'),
+                collection_formats=collection_formats)
             logger.info('Connector added')
             logger.info(add_connector_response)
         except ApiException as e:
@@ -45,23 +60,15 @@ class CloudOneConnector:
             logger.info(e)
 
     def delete_connector(self, aws_account_id):
-        connector_id = self.get_connector_id(aws_account_id)
         try:
-            self.connectorClient.delete_aws_connector(connector_id, self.apiVersion)
-            logger.info('Connector removed')
+            headers = {
+                'Api-Version': 'v1',
+                'Authentication': f'ApiKey {self.configuration.api_key}'
+            }
+            delete_connector_response = self.connectorClient.api_client.call_api(f'cloudaccounts/aws/{aws_account_id}', method="DELETE", headers_params=headers)
+            logger.info('Connector deleted')
+            logger.info(delete_connector_response)
         except ApiException as e:
-            logger.info(e)
-        except Exception as e:
-            logger.info(e)
-
-    def get_connector_id(self, aws_account_id):
-        search_filter = deepsecurity.SearchFilter(
-            search_criteria={"fieldName": "accountId", "stringValue": aws_account_id})
-        try:
-            connectors = self.connectorClient.search_aws_connectors(self.apiVersion, search_filter=search_filter)
-            logger.info(f"Connector ID {connectors.aws_connectors[0].id} found for accountid {aws_account_id}")
-            return connectors.aws_connectors[0].id
-        except ApiException as e:
-            logger.info("An exception occurred when calling AWSConnectorSettingsApi.list_aws_connector_settings: %s\n" % e)
+            logger.info(f"Exception when calling AWSConnectorsApi.delete_aws_connector: {e}")
         except Exception as e:
             logger.info(e)
